6d470484adcf097321a7886cdea28cd1407ee8cc
Alex <alexeyvolskiy@gmail.com>
2019-12-09 23:27:16 +0300

    Reports. Add report for tickets in excel file.

--- BookingOffice/Forms/MyOrdersForm.Designer.cs
+++ BookingOffice/Forms/MyOrdersForm.Designer.cs
@@ -32,6 +32,7 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.button1 = new System.Windows.Forms.Button();
 			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.button2 = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -70,11 +71,22 @@
 			this.dataGridView1.Size = new System.Drawing.Size(643, 340);
 			this.dataGridView1.TabIndex = 3;
 			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(130, 71);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(108, 23);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Сохранить в файл";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
 			// MyOrdersForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(690, 464);
+			this.Controls.Add(this.button2);
 			this.Controls.Add(this.dataGridView1);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.label1);
@@ -93,5 +105,6 @@
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button button2;
 	}
 }
--- BookingOffice/Forms/MyOrdersForm.cs
+++ BookingOffice/Forms/MyOrdersForm.cs
@@ -1,5 +1,6 @@
 ﻿using BookingOffice.Entities;
 using BookingOffice.Utils;
+using ClosedXML.Excel;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -16,21 +17,49 @@ namespace BookingOffice.Forms
 	public partial class MyOrdersForm : Form
 	{
 		private Context context;
-
+		private Ticket[] arrayFlights;
 		public MyOrdersForm()
 		{
 			InitializeComponent();
 			context = new Context();
 			context.Ticket.Load();
 			context.Customers.Load();
+			button2.Enabled = false;
 		}
 
 		private void button1_Click(object sender, EventArgs e)
 		{
 			var passNumber = this.passNumber.Text;
 			var customer = context.Customers.Where(x => x.PassportCode == passNumber).FirstOrDefault() as Customer;
-			var flights = context.Ticket.Where(x => x.CustomerId == customer.Id);
-			dataGridView1.DataSource = flights.ToArray();
+			var tickets = context.Ticket.Where(x => x.CustomerId == customer.Id);
+			arrayFlights = tickets.ToArray();
+			dataGridView1.DataSource = arrayFlights;
+			if (dataGridView1.DataSource != null)
+			{
+				button2.Enabled = true;
+			}
+		}
+		private void button2_Click(object sender, EventArgs e)
+		{
+				XLWorkbook workbook = new XLWorkbook();
+				var worksheet = workbook.Worksheets.Add("Билет");
+				worksheet.Cell("A" + 1).Value = "ID БИЛЕТА";
+				worksheet.Cell("B" + 1).Value = "ID ПОЛЕТА";
+				worksheet.Cell("C" + 1).Value = "ID ПАССАЖИРА";
+				worksheet.Cell("D" + 1).Value = "Стоимость";
+				worksheet.Cell("E" + 1).Value = "Дата покупки";
+				worksheet.Cell("F" + 1).Value = "Класс полета";
+				worksheet.ColumnWidth = 40;
+				for (int i = 0; i < arrayFlights.Length; i++)
+				{
+					worksheet.Cell("A" + $"{i + 2}").Value = arrayFlights[i].Id;
+					worksheet.Cell("B" + $"{i + 2}").Value = arrayFlights[i].FlightId;
+					worksheet.Cell("C" + $"{i + 2}").Value = arrayFlights[i].CustomerId;
+					worksheet.Cell("D" + $"{i + 2}").Value = arrayFlights[i].Price;
+					worksheet.Cell("E" + $"{i + 2}").Value = arrayFlights[i].DateOfPushcare;
+					worksheet.Cell("F" + $"{i + 2}").Value = arrayFlights[i].FlightClass;
+				}
+				workbook.SaveAs(@"Ticket.xlsx");
 		}
 	}
 }
